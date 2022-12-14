23ff300e32a574c7cbb4a2989fd35d172bf6cc91
Eloy Vigil <112999750+EloyVigil@users.noreply.github.com>
2022-12-13 16:32:26 -0800

    Add files via upload

---
+++ nested_dictionary_lists/nestedDictionaryLists.py
@@ -0,0 +1,98 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+# 1.1
+x[1][0]=15;
+print(x);
+
+#1.2
+students[0]['last_name'] = 'Bryant';
+print(students);
+
+#1.3
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory);
+
+#1.4
+z[0]['y']=30
+print(z)
+
+#2
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+def iterateDictionary(students): 
+    for dictionary in students:
+        for key in dictionary:
+            print(f"{key} - {dictionary[key]}")
+
+print(iterateDictionary(students))
+
+#3
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+
+def iterateDictionary2(students, key): 
+    for dictionary in range(len(students)):
+        print(students[dictionary][key])
+
+iterateDictionary2(students, 'first_name')
+iterateDictionary2(students, 'last_name')
+
+
+#4
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def printInfo(dojo):
+    for i in dojo:
+        print(len(dojo[i]),i.upper())
+        for j in dojo[i]:
+            print(j)
+
+printInfo(dojo)
+
+
+# output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
+
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
