d7639ec2e20ba4cba9bf793dfc4c51a15bf8d124
Eloy Vigil <112999750+EloyVigil@users.noreply.github.com>
2022-12-15 07:49:16 -0800

    Add files via upload

---
+++ Basketball Dictionaries/basketball.py
@@ -0,0 +1,80 @@
+from tkinter import Variable
+
+
+class Player:
+    def __init__(self, dict):
+        self.name = dict["name"]
+        self.age = dict["age"]
+        self.position = dict["position"]
+        self.team = dict["team"]
+
+kevin = {
+    	"name": "Kevin Durant", 
+    	"age":34, 
+    	"position": "small forward", 
+    	"team": "Brooklyn Nets"
+}
+jason = {
+    	"name": "Jason Tatum", 
+    	"age":24, 
+    	"position": "small forward", 
+    	"team": "Boston Celtics"
+}
+kyrie = {
+    	"name": "Kyrie Irving", 
+    	"age":32,
+        "position": "Point Guard", 
+    	"team": "Brooklyn Nets"
+}
+    
+# Create your Player instances here!
+player_kevin = Player(kevin)
+print(player_kevin.age)
+player_jason = Player(jason)
+print(player_jason.position)
+player_kyrie = Player(kyrie)
+print(player_kyrie.team)
+
+players = [
+    {
+        "name": "Kevin Durant", 
+        "age":34, 
+        "position": "small forward", 
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Jason Tatum", 
+        "age":24, 
+        "position": "small forward", 
+        "team": "Boston Celtics"
+    },
+    {
+        "name": "Kyrie Irving", 
+        "age":32,
+        "position": "Point Guard", 
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Damian Lillard", 
+        "age":33,
+        "position": "Point Guard", 
+        "team": "Portland Trailblazers"
+    },
+    {
+        "name": "Joel Embiid", 
+        "age":32,
+        "position": "Power Foward", 
+        "team": "Philidelphia 76ers"
+    },
+    {
+        "name": "DeMar DeRozan",
+        "age": 32,
+        "position": "Shooting Guard",
+        "team": "Chicago Bulls"
+    }
+]
+
+for i in range(len(players)):
+    new_team = []
+    new_team += [players[i]]
+    print(new_team)
